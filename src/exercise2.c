ac0b3b67f37de0ac2c98aaa964bab53a2ffac1ce
ksenya513 <karpickaamasya@gmail.com>
2025-10-07 02:10:02 +0700

    blaablaaaavlaaa

--- src/exercise2.c
+++ src/exercise2.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
 
-int main(int argc, char** argv) {
-    printf("Hello World!");
+int main(int argc,char** argv) {
+    float array[10];
+    for (int i = 0; i < 10; i++) {
+        scanf("%f", &array[i]);
+    }
+    for (int i = 9; i >= 0; i--) {
+        printf("%.2f ", numbers[i]);
+    }
+    printf("\n");
     return 0;
 }

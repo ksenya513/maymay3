add258e021b609664034cbe11a9691b46a18906d
ksenya513 <karpickaamasya@gmail.com>
2025-10-07 02:46:39 +0700

    blablablaaaaa

--- src/exercise2.c
+++ src/exercise2.c
@@ -4,10 +4,10 @@ int main(int argc,char** argv) {
     float array[10];
     for (int i = 0; i < 10; i++) {
         scanf("%f", &array[i]);
-    }
+    };
     for (int i = 9; i >= 0; i--) {
         printf("%.2f ", array[i]);
-    }
+    };
     printf("\n");
     return 0;
 }

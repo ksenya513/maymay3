cb5492dc1d1ec7ed481f5898aa057b16c1e60bc6
ksenya513 <karpickaamasya@gmail.com>
2025-10-28 15:00:53 +0700

    Update exercise4.c

--- src/exercise4.c
+++ src/exercise4.c
@@ -1,6 +1,18 @@
 #include <stdio.h>
-
+#include <stdlib.h>
 int main(int argc, char** argv) {
-    printf("Hello World!");
+double array[10];
+double X = atof(argv[1]);
+int K = atoi(argv[0]);
+  for (int i = 0;i<10;i++){
+        scanf("%lf",&array[i]);
+    }
+for(int i=9;i>K;i--){
+    array[i] = array[i-1];
+}
+array[K] = X;
+ for (int i=0;i<10;i++){
+        printf("%lf\n",array[i]);
+    }
     return 0;
 }

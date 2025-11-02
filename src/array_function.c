a906fc4ea71086e0f1e9d168017300a8d637fd6f
ksenya513 <karpickaamasya@gmail.com>
2025-11-02 15:47:13 +0700

    fix8

--- src/array_function.c
+++ src/array_function.c
@@ -1,11 +1,10 @@
 #include "../include/array_function.h"
 
 void join_int_arrays(int *src1, size_t size_src1, int *src2, size_t size_src2, int *dest) {
-int i = 0;
-for(i;i<size_src1;i++) {
+for(int i=0;i<size_src1;i++) {
     dest[i] = src1[i];
 };
-for(i;i<size_src2;i++) {
+for(int i=0;i<size_src2;i++) {
     dest[i+size_src1] = src2[i];
 };
 }

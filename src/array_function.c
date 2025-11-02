143e0bc4d8f12b3dcc062021f0f5a876c4a38c87
ksenya513 <karpickaamasya@gmail.com>
2025-11-02 15:42:00 +0700

    Update array_function.c

--- src/array_function.c
+++ src/array_function.c
@@ -4,12 +4,6 @@
 
 void join_int_arrays(int *src1, size_t size_src1, int *src2, size_t size_src2, int *dest) {
 int i = 0;
-for(i;i<size_src1;i++) {
-    scanf("%d",&src1[i]);
-    }
-for(i;i<size_src2;i++) {
-        scanf("%d",&src2[i]);
-    }
 int* dest[size_src1+size_src2];
 for(i;i<size_src1;i++) {
     dest[i] = src1[i];

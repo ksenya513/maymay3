68bceaae5e5cf04f38c372bf30a86e209ffc1cab
ksenya513 <karpickaamasya@gmail.com>
2025-11-04 20:51:09 +0700

    Update array_function.c

--- src/array_function.c
+++ src/array_function.c
@@ -10,19 +10,7 @@ for(int i=0;i<size_src2;i++) {
 }
 
 void join_and_sort_int_arrays(int* src1, size_t size_src1, int* src2, size_t size_src2, int* dest) {
-    for(int i=0;i<size_src1;i++) {
-    dest[i] = src1[i];
-};
-for(int i=0;i<size_src2;i++) {
-    dest[i+size_src1] = src2[i];
-};
-for(int i=0;i<size_src1+size_src2 -1;i++){
-        if (dest[i]>dest[i+1]){
-            int a=dest[i];
-            dest[i]=dest[i+1];
-            dest[i+1]=a;
-            i=-1;
-        }
+
     
 }
 

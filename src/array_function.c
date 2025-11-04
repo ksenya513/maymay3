d0c29ae8407b2d77bb788b4771c4e4b9a5697016
ksenya513 <karpickaamasya@gmail.com>
2025-11-04 21:23:15 +0700

    fix13

--- src/array_function.c
+++ src/array_function.c
@@ -18,12 +18,12 @@ for(size_t i=0;i<size_src2;i++) {
 }
 size_t n=0;
 while(n<(size_src1+size_src2)-1){
-    n=0;
     for(size_t i=0;i<size_src1+size_src2 -1;i++){
         if (dest[i]>dest[i+1]){
             int a=dest[i];
             dest[i]=dest[i+1];
             dest[i+1]=a;
+            n=0;
         }else {
         n+=1;
         }

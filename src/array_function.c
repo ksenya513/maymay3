9fb969368879ec1260f2daf250e31cf6aeec269c
ksenya513 <karpickaamasya@gmail.com>
2025-11-04 22:16:08 +0700

    Update array_function.c

--- src/array_function.c
+++ src/array_function.c
@@ -17,20 +17,21 @@ for(size_t i=0;i<size_src2;i++) {
     dest[i+size_src1] = src2[i];
 }
 size_t n=0;
-    for(size_t i=0;i<size_src1+size_src2 -1;i++){
-        n=0;
-        for (size_t o=0;0<size_src1+size_src2 -1-i;o++){
-            if (dest[i]>dest[i+1]){
-                int a=dest[i];
-                dest[i]=dest[i+1];
-                dest[i+1]=a;
-                n+=1;
-            }
+while(n<(size_src1+size_src2)-1){
+    n=0;
+    size_t i=0;
+    for(i;i<size_src1+size_src2 -1;i++){
+        if (dest[i]>dest[i+1]){
+            int a=dest[i];
+            dest[i]=dest[i+1];
+            dest[i+1]=a;
+            i=0;
+        }else {
+        n+=1;
         }
     }
-    if (n==0){
-        break;    
-    }
+    
+}
 }
 
 void get_min_and_max_from_int_array(int* src, size_t size, int* min, int* max) {}

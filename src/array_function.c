e5066c44fecd64a42cb5659f061909289d8cc629
ksenya513 <karpickaamasya@gmail.com>
2025-11-04 20:26:50 +0700

    fix10

--- src/array_function.c
+++ src/array_function.c
@@ -10,19 +10,20 @@ for(int i=0;i<size_src2;i++) {
 }
 
 void join_and_sort_int_arrays(int* src1, size_t size_src1, int* src2, size_t size_src2, int* dest) {
-    for(int i=0;i<size_src1+size_src2;i++) {
-    if (src1[i]<src2[i]){
-        while (src1[i]<src2[i]){
-        dest[i] = src1[i]; 
+    for(int i=0;i<size_src1;i++) {
+    dest[i] = src1[i];
+};
+for(int i=0;i<size_src2;i++) {
+    dest[i+size_src1] = src2[i];
+};
+for(int i=0;i<size_src1+size_src2;i++){
+    while (dest[size_src1]>dest[size_src1+1]){
+        if (dest[i]>dest[i+1]){
+            int a=dest[i];
+            dest[i]=dest[i+1];
+            dest[i+1]=a;
         }
-    } else if (src1[i]==src2[i]){
-        while(src1[i]==src2[i]){
-        dest[i]=src1[i];
-        }
-    }else {
-        while(src1[i]>src2[i]){
-            dest[i]=src2[i];
-        }
+
     }
     
 }

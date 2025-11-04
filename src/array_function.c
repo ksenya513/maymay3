6c81062be3dbc9de5cac76513bf0047800c6fa75
ksenya513 <karpickaamasya@gmail.com>
2025-11-04 21:16:18 +0700

    fix12

--- src/array_function.c
+++ src/array_function.c
@@ -12,12 +12,12 @@ for(int i=0;i<size_src2;i++) {
 void join_and_sort_int_arrays(int* src1, size_t size_src1, int* src2, size_t size_src2, int* dest) {
     for(size_t i=0;i<size_src1;i++) {
     dest[i] = src1[i];
-};
+}
 for(size_t i=0;i<size_src2;i++) {
     dest[i+size_src1] = src2[i];
-};
-int n=0;
-while(n<size_src1+size_src2 -1){
+}
+size_t n=0;
+while(n<(size_src1+size_src2)-1){
     n=0;
     for(size_t i=0;i<size_src1+size_src2 -1;i++){
         if (dest[i]>dest[i+1]){

8775384f1b905f96ca0a14a9cc1cd4707318e7a7
ksenya513 <karpickaamasya@gmail.com>
2025-11-04 21:51:37 +0700

    Update array_function.c

--- src/array_function.c
+++ src/array_function.c
@@ -17,15 +17,14 @@ for(size_t i=0;i<size_src2;i++) {
     dest[i+size_src1] = src2[i];
 }
 size_t n=0;
-while(n<(size_src1+size_src2)-1){
+while(n!=1){
     for(size_t i=0;i<size_src1+size_src2 -1;i++){
+        n=1;
         if (dest[i]>dest[i+1]){
             int a=dest[i];
             dest[i]=dest[i+1];
             dest[i+1]=a;
-            n=1;
-        }else if(n==0){
-            break;
+            n=0;
         }
         }
     }
